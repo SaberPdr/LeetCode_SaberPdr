5a3ed4ffb35b679098c688efe54c80d4a4199c87
qiuqiu <1184228534@qq.com>
2018-01-25 17:34:34 +0800

    Update 1.Two Sum.cpp

--- 1.Two Sum.cpp
+++ 1.Two Sum.cpp
@@ -1,16 +1,20 @@
 //好的解法:来自LeetCode论坛
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
         unordered_map<int, int> mapping;
         //map<int, int> mapping;
         vector<int> result;
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) 
+	{
             mapping[nums[i]] = i;
         }
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) 
+	{
             const int gap = target - nums[i];
-            if (mapping.find(gap) != mapping.end() && mapping[gap] > i) {
+            if (mapping.find(gap) != mapping.end() && mapping[gap] > i) 
+	    {
                 result.push_back(i );
                 result.push_back(mapping[gap]);
                 break;
@@ -24,7 +28,8 @@ public:
 /********************
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<int> twoSum(vector<int>& numbers, int target) 
+    {
         vector<int> result;
 	    vector<int> num2 = numbers;
         int n = num2.size();
@@ -45,11 +50,35 @@ public:
 
         vector<int>::iterator finded_a = find(numbers.begin(), numbers.end(), num2[a]);
         vector<int>::iterator finded_b = find(numbers.begin(), numbers.end(), num2[b]);  
-//在出现[3,3] 6这样的数据时会有问题, 因为find函数找的只是第一个出现的位置
+//在出现[3,3] 6这样的数据时会有问题, 因为find函数找的只是第一个出现的位置,所以这个解法是错误的
         result.push_back(numbers.end() - finded_b);
         result.push_back(numbers.end() - finded_a);
         
 	    return result;
     }
+};
+************************/
+//还有最朴素的O(n)复杂度的,虽然慢但是一定对
+/***********************
+class Solution {
+vector<int> twoSum(vector<int> &numbers, int target)
+        {
+                vector<int> result;
+                int n = numbers.size();
+                for(int i = 0; i < n; i++)
+                {
+                        int numToFind = target - numbers[i];
+                        for(int j = i+1; j < n; j++)
+                        {
+                                if(numbers[j] == numToFind)
+                                {
+                                        result.push_back(i);
+                                        result.push_back(j);
+                                        return result;
+                                }
+                        }
+                }
+                return result;
+        }
 };
 ************************/
