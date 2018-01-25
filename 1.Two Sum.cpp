5ca9b6632672659610824c913b67d4043f09efb3
qiuqiu <1184228534@qq.com>
2018-01-25 17:35:23 +0800

    Update 1.Two Sum.cpp

--- 1.Two Sum.cpp
+++ 1.Two Sum.cpp
@@ -1,28 +1,45 @@
 //好的解法:来自LeetCode论坛
-class Solution {
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
-        unordered_map<int, int> mapping;
-        //map<int, int> mapping;
-        vector<int> result;
-        for (int i = 0; i < nums.size(); i++) 
-	{
-            mapping[nums[i]] = i;
+        vector<int> twoSum(vector<int> &numbers, int target)
+        {
+                //Key is the number and value is its index in the vector.
+                unordered_map<int, int> hash;
+                vector<int> result;
+                for (int i = 0; i < numbers.size(); i++)
+                {
+                        int numberToFind = target - numbers[i];
+
+                        //if numberToFind is found in map, return them
+                        if (hash.find(numberToFind) != hash.end())
+                        {
+                                result.push_back(hash[numberToFind]);
+                                result.push_back(i);
+                                return result;
+                        }
+
+                        //number was not found. Put it in the map.
+                        hash[numbers[i]] = i;
+                }
+                return result;
         }
-        for (int i = 0; i < nums.size(); i++) 
-	{
-            const int gap = target - nums[i];
-            if (mapping.find(gap) != mapping.end() && mapping[gap] > i) 
-	    {
-                result.push_back(i );
-                result.push_back(mapping[gap]);
-                break;
-            }
-        }
-        return result;
-    }
 };
+
+int main()
+{
+        Solution S;
+        vector<int> numbers = {3,3};
+        int target = 6;
+        vector<int> anwsers = S.twoSum(numbers,target);
+        cout << anwsers[0] << "," << anwsers[1] << endl;
+        return 0;
+}
+
 
 //最开始我的想法
 /********************
